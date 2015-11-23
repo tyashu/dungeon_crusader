cc3f2a84ed6e40998ab3d583a260ec7230658d37
田邉 康志 <tyashushi@gmail.com>
2015-11-23 20:52:51 +0900

    [ADD] 画面サイズによってミニマップ表示を修正する処理追加

---
+++ Assets/Scripts/MiniMapCameraSize.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMapCameraSize : MonoBehaviour {
+
+	public Camera Camera;
+
+	// Use this for initialization
+	void Start () {
+		float baseAspect = 16f / 9f;
+		float currentAspect = Screen.height / Screen.width;
+		float fix = baseAspect / currentAspect;
+
+		float x = 0.12f*fix;
+		float y = 0.05f;
+		float w = 1 - x * 2;
+		float h = 0.27f;
+
+		Camera.rect = new Rect(x,y,w,h);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
